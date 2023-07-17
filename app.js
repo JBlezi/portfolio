d15bd3992e10abb980093ae00b9cb0b03fa563e6
Julius Blezinger <jgoldappel@gmail.com>
2023-07-17 23:28:59 +0200

    customize thumbnail for youtube video

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: "Digital Designer & Web Developer",
-      description: "My goal is to make your business shine and bring out its true potential, by giving you a website that is as great as your business.",
-      words: [],
-      isLoaded: false,
+      showVideo: false,
+      videoUrl: 'https://www.youtube.com/embed/NYzqGc2xBaw?autoplay=1&start=3921',
     };
   },
-  mounted() {
-    const words = this.title.split(" ");
-    let delay = 0;
-    for (const [index, word] of words.entries()) {
-      setTimeout(() => {
-        this.words.push(word + " ");
-        if (index === words.length - 1) {
-          this.$nextTick(() => {
-            this.isLoaded = true; // Set the flag to indicate the paragraph can be loaded
-          });
-        }
-      }, delay);
-      delay += 300; // Adjust the delay between each word (in milliseconds)
-    }
+  methods: {
+    playVideo() {
+      this.showVideo = true;
+    },
   },
 });
-app.mount("#app");
+
+app.mount('#app');
