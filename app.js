ec7e4d01ea0bcff00e00b0dae766636f60eb392b
Julius Blezinger <jgoldappel@gmail.com>
2023-07-20 14:46:53 +0200

    fixes small screen issues

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const app = Vue.createApp({
     },
   },
   mounted() {
+    if (window.matchMedia("(min-width: 767px)").matches){
+
+
     gsap.registerPlugin(ScrollTrigger);
 
     let sections = gsap.utils.toArray(".horizontal-scroll");
@@ -48,44 +51,45 @@ const app = Vue.createApp({
     });
 
     let scrollTween = gsap.to(sections, {
-        xPercent: updateXPercent(),
-        ease: "none", // <-- IMPORTANT!
-        scrollTrigger: {
-          trigger: ".scroll-container",
-          start: "top top",
-          pin: true,
-          scrub: true,
-          //snap: directionalSnap(1 / (sections.length - 1)),
-          end: "+=3000"
-        }
-      });
-
-
-    // green section
-    ScrollTrigger.create({
-
-      containerAnimation: scrollTween,
-
-      onEnter: () => console.log("enter"),
-      onLeave: () => console.log("leave"),
-      onEnterBack: () => console.log("enterBack"),
-      onLeaveBack: () => console.log("leaveBack"),
-      onToggle: self => console.log("active", self.isActive),
-      id: "4"
+      xPercent: updateXPercent(),
+      ease: "none", // <-- IMPORTANT!
+      scrollTrigger: {
+        trigger: ".scroll-container",
+        start: "top top",
+        pin: true,
+        scrub: true,
+        //snap: directionalSnap(1 / (sections.length - 1)),
+        end: "+=3000"
+      }
     });
 
-    // only show the relevant section's markers at any given time
-    gsap.set(".gsap-marker-start, .gsap-marker-end, .gsap-marker-scroller-start, .gsap-marker-scroller-end", {autoAlpha: 0});
-    ["first","second","third","last"].forEach((triggerClass, i) => {
+
+      // green section
       ScrollTrigger.create({
-        trigger: "." + triggerClass,
+
         containerAnimation: scrollTween,
-        start: "left 30%",
-        end: i === 3 ? "right right" : "right 30%",
-        markers: false,
-        onToggle: self => gsap.to(".marker-" + (i+1), {duration: 0.25, autoAlpha: self.isActive ? 1 : 0})
+
+        onEnter: () => console.log("enter"),
+        onLeave: () => console.log("leave"),
+        onEnterBack: () => console.log("enterBack"),
+        onLeaveBack: () => console.log("leaveBack"),
+        onToggle: self => console.log("active", self.isActive),
+        id: "4"
       });
-    });
+
+      // only show the relevant section's markers at any given time
+      gsap.set(".gsap-marker-start, .gsap-marker-end, .gsap-marker-scroller-start, .gsap-marker-scroller-end", {autoAlpha: 0});
+      ["first","second","third","last"].forEach((triggerClass, i) => {
+        ScrollTrigger.create({
+          trigger: "." + triggerClass,
+          containerAnimation: scrollTween,
+          start: "left 30%",
+          end: i === 3 ? "right right" : "right 30%",
+          markers: false,
+          onToggle: self => gsap.to(".marker-" + (i+1), {duration: 0.25, autoAlpha: self.isActive ? 1 : 0})
+        });
+      });
+    }
   }
 });
 
