b8cb899fea4eb1064222e10f6e441206f90f4af3
Julius Blezinger <jgoldappel@gmail.com>
2023-07-20 11:23:36 +0200

    makes horizontal scroll distance responsive

--- app.js
+++ app.js
@@ -15,11 +15,44 @@ const app = Vue.createApp({
 
     let sections = gsap.utils.toArray(".horizontal-scroll");
 
+    const updateXPercent = () => {
+      let xPercent;
+      if (window.matchMedia("(max-width: 1000px)").matches) {
+        // For small screens (max-width: 767px)
+        xPercent = -78 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1100px)").matches){
+        xPercent = -77.5 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1200px)").matches){
+        xPercent = -77 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1300px)").matches){
+        xPercent = -76.5 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1400px)").matches){
+        xPercent = -76 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1500px)").matches){
+        xPercent = -75.5 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1600px)").matches){
+        xPercent = -75 * (sections.length - 1);
+      } else if (window.matchMedia("(max-width: 1700px)").matches){
+        xPercent = -74.5 * (sections.length - 1);
+      } else {
+        // For larger screens
+        xPercent = -73 * (sections.length - 1);
+      }
+      return xPercent;
+    };
+
+    // Update the xPercent value whenever the window is resized
+    window.addEventListener("resize", () => {
+      scrollTween.vars.xPercent = updateXPercent();
+      scrollTween.invalidate(); // Invalidate the animation for smooth updates
+    });
+
     let scrollTween = gsap.to(sections, {
-        xPercent: -73 * (sections.length - 1),
+        xPercent: updateXPercent(),
         ease: "none", // <-- IMPORTANT!
         scrollTrigger: {
           trigger: ".scroll-container",
+          start: "top top",
           pin: true,
           scrub: true,
           //snap: directionalSnap(1 / (sections.length - 1)),
@@ -27,9 +60,10 @@ const app = Vue.createApp({
         }
       });
 
+
     // green section
     ScrollTrigger.create({
-      trigger: ".last",
+
       containerAnimation: scrollTween,
 
       onEnter: () => console.log("enter"),
